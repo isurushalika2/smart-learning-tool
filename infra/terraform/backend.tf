7dc7c5df0a782e4ecb8509855cd80882e6db804e
Isuru Shalika <isurushalika@Isurus-MacBook-Pro.local>
2025-08-21 00:37:20 +0530

    Add terraform to creste aws resources through ci/cd

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-109598918608-us-east-1"
-    key            = "envs/prod/terraform.tfstate"
+    key            = "envs/prod/terraform.tfstate"  # choose any path you like
     region         = "us-east-1"
     dynamodb_table = "tf-state-locks"
     encrypt        = true
